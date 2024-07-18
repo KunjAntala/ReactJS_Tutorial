373a4a9f3425005c08958b60568b71c8de822bdb
KunjGit1 <138096211+KunjGit1@users.noreply.github.com>
2024-07-18 13:38:17 +0530

    new commit

--- password_generator/src/App.jsx
+++ password_generator/src/App.jsx
@@ -1,35 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useEffect, useRef, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8);
+  const [numAllowed,setNumAllowed] = useState(false);
+  const [charAllowed,setCharAllowed] = useState(false);
+  const [password,setPassword] = useState();
+
+  // useRef hook
+  const passwordRef = useRef(null)
+
+  // useCallback hook
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if (numAllowed) {
+      str += "0123456789"
+    }
+    if (charAllowed) {
+      str += "!@#$%^&*"
+    }
+
+    for (let i = 1; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length )
+      pass += str.charAt(char)
+    }
+
+    setPassword(pass)
+
+  },[length,numAllowed,charAllowed])
+
+  const copyPassword = useCallback(() => {
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  // useEffect hook
+  useEffect(() => {
+    passwordGenerator()
+  }, [length,numAllowed,charAllowed,passwordGenerator])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800'>
+      <h1 className='text-2xl text-center text-white'>Password Generator</h1>
+        <div className="flex shadow-md rounded-lg overflow-hidden my-4">
+          <input 
+            type="text"
+            value={password}
+            className='outline-none w-full px-3 py-1'
+            placeholder='Password'
+            readOnly
+            ref={passwordRef}
+           />
+           <button onClick={copyPassword}
+            className='outline-none text-white bg-blue-700 px-3 py-0.5 shrink-0 hover:bg-blue-900'>
+              Copy
+           </button>
+        </div>
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input 
+              type="range"
+              min={8}
+              max={12}
+              value={length}
+              className='cursor-pointer'
+              onChange={(events) => {setLength(events.target.value)}}
+            />
+            <label >Length : {length}</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input 
+              type="checkbox"
+              defaultChecked={numAllowed}
+              id='numberInput'
+              onChange={() => {
+                setNumAllowed((prev) => !prev);
+              }}
+            />
+            <label>Number</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input 
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id='charInput'
+              onChange={() => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label>Character</label>
+          </div>
+        </div>
+    </div>
   )
 }
 
 export default App
+ 
